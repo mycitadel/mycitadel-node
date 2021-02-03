2b6ec243650409735493e8c4f499d11bb7c0e7e5
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-02-03 19:52:10 +0100

    Revering build script changes

--- packages/MyCitadelKit/build.sh
+++ packages/MyCitadelKit/build.sh
@@ -1,5 +1,5 @@
-xcodebuild archive -scheme MyCitadelKit -destination "generic/platform=iOS" -archivePath ../../artifacts/MyCitadelKit-iOS SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=NO &&
-  xcodebuild archive -scheme MyCitadelKit -destination "generic/platform=iOS Simulator" -archivePath ../../artifacts/MyCitadelKit-iOS-Sim SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=NO VALID_ARCHS=x86_64 &&
+xcodebuild archive -scheme MyCitadelKit -destination "generic/platform=iOS" -archivePath ../../artifacts/MyCitadelKit-iOS SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES &&
+  xcodebuild archive -scheme MyCitadelKit -destination "generic/platform=iOS Simulator" -archivePath ../../artifacts/MyCitadelKit-iOS-Sim SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES VALID_ARCHS=x86_64 &&
   cd ../../artifacts &&
   rm -rf ./MyCitadelKit.xcframework &&
   xcodebuild -create-xcframework -framework MyCitadelKit-iOS.xcarchive/Products/Library/Frameworks/MyCitadelKit.framework -framework MyCitadelKit-iOS-Sim.xcarchive/Products/Library/Frameworks/MyCitadelKit.framework -output ./MyCitadelKit.xcframework
