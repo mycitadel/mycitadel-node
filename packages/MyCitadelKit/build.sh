b4fd1cfb7a10dcf94c635b9aa0e29436a63f4b3d
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-03-06 14:20:58 +0100

    Refactoring MyCitadelKit directory structure

--- packages/MyCitadelKit/build.sh
+++ packages/MyCitadelKit/build.sh
@@ -1,6 +1,6 @@
-xcodebuild archive -scheme MyCitadelKit -destination "generic/platform=iOS" -archivePath ../../artifacts/MyCitadelKit-iOS SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES &&
-  xcodebuild archive -scheme MyCitadelKit -destination "generic/platform=iOS Simulator" -archivePath ../../artifacts/MyCitadelKit-iOS-Sim SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES VALID_ARCHS=x86_64 &&
-  xcodebuild archive -scheme CitadelKit -destination "generic/platform=macOS" -archivePath ../../artifacts/MyCitadelKit-macOS SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES VALID_ARCHS=x86_64 &&
+xcodebuild archive -scheme iOS -destination "generic/platform=iOS" -archivePath ../../artifacts/MyCitadelKit-iOS SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES &&
+  xcodebuild archive -scheme iOS -destination "generic/platform=iOS Simulator" -archivePath ../../artifacts/MyCitadelKit-iOS-Sim SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES VALID_ARCHS=x86_64 &&
+  xcodebuild archive -scheme macOS -destination "generic/platform=macOS" -archivePath ../../artifacts/MyCitadelKit-macOS SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES VALID_ARCHS=x86_64 &&
   cd ../../artifacts &&
   rm -rf ./MyCitadelKit.xcframework &&
   xcodebuild -create-xcframework -framework MyCitadelKit-iOS.xcarchive/Products/Library/Frameworks/MyCitadelKit.framework \
